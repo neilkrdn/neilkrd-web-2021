758736dba64a9f7e44ee6fb5c6a0ca2e65cc4492
Neil Kardan <neilkardan@Neils-MacBook-Pro.local>
2021-12-31 13:36:39 -0800

    Added skeleton sections for Projects, Blog, and About. Added on hover enlargen, more shadows, fixed centering, etc.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ function App() {
          <div className="box">
            <div className="tagline">
             Neil Kardan
+            <div className="subtag">
+              Designer | Developer | Scholar
+            </div>
            </div>
+           
            <img className="coverImg" src={require('./Assets/coverImg.jpg')} alt="picture of neil"/>
          </div>
        </div>
@@ -24,7 +28,9 @@ function App() {
         <div className="projects">
           <div className="row">
               <div className="column left">
+                <a href="https://github.com/neilkrdn/BruinSource">
                   <img className="projectLogo" src={require('./Assets/ProjectIcons/bruinsource_logo.png')} alt="bruinsource logo"/>
+                </a>
               </div>
               <div className="column right">
                   A platform for students to join and collaborate on OpenSource projects within their interests,
@@ -32,7 +38,31 @@ function App() {
               </div>
           </div>
         </div>
-
+        <div className="box2">
+          <div className="headcard">
+            Blog
+          </div>
+        </div>
+        <p> Coming soon </p>
+        <div className="box2">
+          <div className="headcard">
+            About
+          </div>
+        </div>
+        <div className="row">
+          <div className="column left">
+            <img className="aboutMe" src={require('./Assets/aboutme.JPG')} alt="picture of neil"/>
+          </div>
+          <div className="column right">
+            <div className="aboutMeDescription">
+              I'm a current student at the University of California: Los Angeles, where I study Computer Science.
+              <br></br><br></br>
+              In my degree, I will be completing a technical breadth in Technology Management and upper divisions in Linguistics.
+              <br></br><br></br>
+              In my free time, I love to read, paint, and stay active. Check out my blog posts for reviews of my recently finished books!
+            </div>
+          </div>
+        </div>
 
         <footer className="footy">
           <a href="https://www.linkedin.com/in/neilkardan">
