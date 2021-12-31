3874b39d35be96e479289b5dfb215605ad115ea0
Neil Kardan <neilkardan@Neils-MacBook-Pro.local>
2021-12-30 17:46:39 -0800

    Fixing tab icon and name; added Project description and icon, set up footer with working links.

--- src/App.js
+++ src/App.js
@@ -16,21 +16,37 @@ function App() {
            <img className="coverImg" src={require('./Assets/coverImg.jpg')} alt="picture of neil"/>
          </div>
        </div>
-        <div className="headcard">
-          Projects
+       <div className="box2">
+          <div className="headcard">
+            Projects
+          </div>
         </div>
-          <div className="project">
-
-              <div className="projectIcon">
-                  BruinSource
+        <div className="projects">
+          <div className="row">
+              <div className="column left">
+                  <img className="projectLogo" src={require('./Assets/ProjectIcons/bruinsource_logo.png')} alt="bruinsource logo"/>
               </div>
-              <div className="projectDescription">
+              <div className="column right">
                   A platform for students to join and collaborate on OpenSource projects within their interests,
                   experience, and skillsets. 
               </div>
+          </div>
         </div>
-    </div>
 
+
+        <footer className="footy">
+          <a href="https://www.linkedin.com/in/neilkardan">
+            <img className="footIcon" src={require('./Assets/FooterIcons/linkedin.png')} alt="linkedin" />
+          </a>
+          <a href="mailto:neilkardan@gmail.com">
+            <img className="footIcon" src={require('./Assets/FooterIcons/mail.png')} alt="mail" /> 
+          </a>
+          <a href="https://www.github.com/neilkrdn">
+            <img className="footIcon" src={require('./Assets/FooterIcons/github.png')} alt="github" /> 
+          </a>
+        </footer>
+
+      </div>
   );
 }
 
